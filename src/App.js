b24eec2d762ea904baea2cfdb3d1f886b21b7ecc
BahodirDev <mydatabases714@gmail.com>
2023-01-15 04:26:06 -0800

    client test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Register from './pages/Register';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import RandomTest from './pages/user/RandomTest';
+import ProtectedRouteUser from './components/ProtectedRouteUser';
+import ProtectedRouteTeacher from './components/ProtectedRouteTeacher';
+import Kreteriya from './pages/teacher/Kreteriya';
+import Category from './pages/teacher/Category';
+import CreateQuestions from './pages/teacher/CreateQuestions';
+import toast, { Toaster } from 'react-hot-toast';
+import UserProfile from './pages/user/UserProfile';
+import MyRating from './pages/user/MyRating';
+import ProtectedRouteAdmin from './components/ProtectedRouteAdmin';
+import AdminDashboard from './pages/admin/AdminDashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/user' element={<ProtectedRouteUser />}>
+            <Route path='test/:categoryId' element={<RandomTest />} />
+            <Route path='profile' element={<UserProfile />} />
+            <Route path='rating' element={<MyRating />} />
+          </Route>
+          <Route path='/teacher' element={<ProtectedRouteTeacher />}>
+            <Route path='dashboard' element={<Kreteriya />} />
+            <Route path='category' element={<Category />} />
+            <Route path='questions' element={<CreateQuestions />} />
+          </Route>
+          <Route path='/admin' element={<ProtectedRouteAdmin />}> 
+            <Route path='dashboard' element={<AdminDashboard />} />
+          </Route>
+        </Routes>
+      </div>
+      <Toaster />
+    </>
+
   );
 }
 
